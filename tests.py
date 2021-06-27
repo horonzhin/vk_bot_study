f8504e9dc3176212ab6f003334df1bc1b19ca9dd
Dmitry Khoronzhin <horonzhin@gmail.com>
2021-06-27 15:55:07 +0300

    test on_event() - ok

--- tests.py
+++ tests.py
@@ -1,10 +1,21 @@
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, ANY
+
+from vk_api.bot_longpoll import VkBotMessageEvent
 
 from bot import Bot
 
 
 class Test1(TestCase):
+    RAW_EVENT = {
+        'type': 'message_new', 'object':
+        {'message': {'date': 1624796253, 'from_id': 6218474, 'id': 116, 'out': 0, 'peer_id': 6218474, 'text': 'тм',
+                    'conversation_message_id': 113, 'fwd_messages': [], 'important': False, 'random_id': 0,
+                    'attachments': [], 'is_hidden': False},
+        'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link', 'callback',
+                        'intent_subscribe', 'intent_unsubscribe'], 'keyboard': True, 'inline_keyboard': True,
+                        'carousel': True, 'lang_id': 0}},
+        'group_id': 205318557, 'event_id': '1d104ee315dc2c2d2ca1322b8aec6b3daf8e1f47'}
 
     def test_run(self):
         count = 5
@@ -23,3 +34,21 @@ class Test1(TestCase):
                 bot.on_event.assert_called()
                 bot.on_event.assert_any_call(obj)
                 assert bot.on_event.call_count == count
+
+    def test_on_event(self):
+        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+
+        send_mock = Mock()
+
+        with patch('bot.vk_api.VkApi'):
+            with patch('bot.VkBotLongPoll'):
+                bot = Bot('', '')
+                bot.api = Mock()
+                bot.api.messages.send = send_mock
+
+                bot.on_event(event)
+
+        send_mock.assert_called_once_with(
+            message=self.RAW_EVENT['object']['message']['text'],
+            random_id=ANY,
+            peer_id=self.RAW_EVENT['object']['message']['peer_id'])
