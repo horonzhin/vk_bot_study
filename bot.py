b6d57fff4d17f69fa62ec14406de7eb69f5cb545
Dmitry Khoronzhin <horonzhin@gmail.com>
2021-07-26 08:37:34 +0300

    Прописал структуру сценариев (intent, scenaio, handler). Прописал логику бота. Добавил комментарии.

--- bot.py
+++ bot.py
@@ -4,34 +4,63 @@ import logging
 import random
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import handlers
+
 try:
     import settings
 except ImportError:
+    # если программист забудет скопировать сеттингс в сеттингсдефолт мы закроем программу и напишем ему об этом
     exit('DO cp settings.py.default settings.py and set token')
 
+# создаем логгер bot, как отдельный объект логирования
 log = logging.getLogger('bot')
 
 
 def configure_logging():
+    # позволяет выводит логеры на консоль (без записи в файл)
     stream_handler = logging.StreamHandler()
+    # формат вывода сообщений "уровень - сообщение"
     stream_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+    # установим уровень логирования. От какого уровня выводить сообщения.
     stream_handler.setLevel(logging.INFO)
+    # применяем stream_handler к нашему логеру bot
     log.addHandler(stream_handler)
 
+    # позволяет записывать логи в файл
     file_handler = logging.FileHandler('bot.log', mode='w', encoding='utf-8')
     file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s',
                                                 datefmt='%d-%m-%Y %H:%M'))
     file_handler.setLevel(logging.DEBUG)
     log.addHandler(file_handler)
 
+    # по умолчанию уровень логирования error, чтобы наши хэндлеры с info и debug попадали куда нужно укажем общий ур-нь
     log.setLevel(logging.DEBUG)
 
 
+# класс отвечающий за находение пользователя на каком-то шаге какого-то сценария
+class UserState:
+    """Состояние пользователя внутри сценария"""
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        # если контекст пустой (None или пустой Dict) то передадим пустой Dict
+        self.context = context or {}
+
+
+# создаем в вк сообщество. В настройках создаем ключ (токен) и включаем LongPoll.
+# В LongPoll включаем нужные события, которые хотим получать.
 class Bot:
     """
     Echo bot for vk.com
     Use Python3.9
+    Поддерживает ответы на впоросы про дату, место проведения и сценарии регистрации:
+    - спрашиваем имя
+    - спрашиваем email
+    - говорим об успешной регистрации
+    Если шаг не пройден, задаем уточняющий вопрос пока шаг не будет пройден
     """
+
     def __init__(self, GROUP_ID, TOKEN):
         """
         :param GROUP_ID: group id from vk group
@@ -40,16 +69,26 @@ class Bot:
         self.group_id = GROUP_ID
         self.token = TOKEN
         self.vk = vk_api.VkApi(token=TOKEN)
+        # существует либо LongPoll (бот спрашивает есть новые события или нет, через определенное время задержки
+        # приходит ответ о том, что есть новые события или нет) либо Callback (присылает уведомления, как только
+        # происходит какое-то событие)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
+        # мотод который позволит боту отвечать пользователю, а не нам
         self.api = self.vk.get_api()
+        # переменная отвечающая за состояние пользователя: находение пользователя на каком-то шаге какого-то сценария
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """ Run bot """
         for event in self.long_poller.listen():
-            # print('Новое сообщение:')
+            # метод listen это бесконечный цикл опрашивания вк на предмет совершения событий. listen это итератор
+            # по-этому по нему можно проходить циклом.
+            # print('Новое сообщение:') проверочный принт
             try:
                 self.on_event(event)
             except Exception:
+                # метод exception логирует ошибку с уровнем error, но помимо сообщения будет
+                # добавлена подробная информация исключения
                 log.exception('Ошибка в обработке события')
 
     def on_event(self, event):
@@ -59,13 +98,93 @@ class Bot:
         :return: None
         """
         if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('Отправляем сообщение назад')
-            # print('Текст:', event.message.text)
-            self.api.messages.send(message=event.message.text,
-                                   random_id=random.randint(0, 2 ** 20),
-                                   peer_id=event.message.peer_id)
-        else:
             log.info('Пока не умеем обрабатывать события такого типа %s', event.type)
+            return
+
+        user_id = event.message.peer_id
+        text = event.message.text
+        # если пользователь в структуре user_states, то продолжаем сценарий, если нет то он вне сценария.
+        # Нужно найти интент, чтобы выдать сразу ответ, либо начать сценарий
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id=user_id, text=text)
+        else:
+            # search intent
+            for intent in settings.INTENTS:
+                # если какой-нибудь токен находится в тексте всерди всех токенов, то запускаем интент
+                if any(token in text for token in intent['tokens']):
+                    # если интент имеет не пустой ответ, значит надо его сообщить и больше ничего не делать,
+                    # если нет, то запустить сценарий
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(message=text_to_send,
+                               # выдергиваем из объекта только текст сообщения
+                               random_id=random.randint(0, 2 ** 20),
+                               # задержка для того, чтобы если одно и тоже сообщение будет отослано несколько
+                               # раз подряд, то пользователь увидит только одно сообщение
+                               peer_id=user_id) # id позователя, чтобы ответ пришел именно ему
+
+    # метод который будет запускать сценарии
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        # первый шаг с которого нужно начать
+        first_step = scenario['first_step']
+        # запускаем этот первый шаг
+        step = scenario['steps'][first_step]
+        # выдаем текст этого шага и сохраняем state
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario, step_name=first_step)
+
+        return text_to_send
+
+    # метод который будет заниматься только продолжение сценария
+    def continue_scenario(self, user_id, text):
+        # нужно понять на каком шаге он находится, прошел ли он этот шаг и либо оставить его на этом шаге
+        # (если не закончил), либо пребросить на следующий.
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        # далее нужно запустить handler. Нужно понять находится ли данный handler в исходном файле handler
+        handler = getattr(handlers, step['handler'])
+        # сверяем handler с тем что написал пользователь
+        if handler(text=text, context=state.context):
+            # если True то следующий шаг
+            next_step = steps[step['next_step']]
+            # после переходна на новый шаг, нужно отправить сообщение из этого шага с контекстом (имя, почта и т.д.)
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                # если у следующего степа есть некс степ, то переходим в него
+                state.step_name = step['next_step']
+            else:
+                # если нет, то заканчиваем сценарий. Т.е. нужно удалить state из хранилища state
+                self.user_states.pop(user_id)
+        else:
+            # если handler не совпал, то остается на текущем шаге и выдать failure_text
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
+
+        #     # если событие типа "новое сообщение" то оно нас интересует
+        #     log.debug('Отправляем сообщение назад')
+        #     # позволит видеть на консоли, как срабатывают разные уровни логирования
+        #     # print('Текст:', event.message.text) проверочный принт
+        #     # метод позволяет боту отвечать пользователю. Вид ответа который получит пользователь
+        #     self.api.messages.send(message=event.message.text,
+        #                            # выдергиваем из объекта только текст сообщения
+        #                            random_id=random.randint(0, 2 ** 20),
+        #                            # задержка для того, чтобы если одно и тоже сообщение будет отослано несколько
+        #                            # раз подряд, то пользователь увидит только одно сообщение
+        #                            peer_id=event.message.peer_id)
+        #                            # id позователя, чтобы ответ пришел именно ему
+        # else:
+        # log.info('Пока не умеем обрабатывать события такого типа %s', event.type)
+        # # если это событие другого типа, которое мы пока не умеем обрабатывать, то логируем это сообщение.
+        # # %s позволяет не форматировать сообщения ниже уровня логирования который мы хотим видеть
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -7,48 +7,89 @@ from bot import Bot
 
 
 class Test1(TestCase):
+    # пишем в сообществе вк любое сообщение смотрим, что приходит нам на консоль и тупо копируем все в переменную,
+    # как пример того, что мы должны получать
     RAW_EVENT = {
         'type': 'message_new', 'object':
-        {'message': {'date': 1624796253, 'from_id': 6218474, 'id': 116, 'out': 0, 'peer_id': 6218474, 'text': 'тм',
-                    'conversation_message_id': 113, 'fwd_messages': [], 'important': False, 'random_id': 0,
-                    'attachments': [], 'is_hidden': False},
-        'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link', 'callback',
-                        'intent_subscribe', 'intent_unsubscribe'], 'keyboard': True, 'inline_keyboard': True,
-                        'carousel': True, 'lang_id': 0}},
+            {'message': {'date': 1624796253, 'from_id': 6218474, 'id': 116, 'out': 0, 'peer_id': 6218474, 'text': 'тм',
+                         'conversation_message_id': 113, 'fwd_messages': [], 'important': False, 'random_id': 0,
+                         'attachments': [], 'is_hidden': False},
+             'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link', 'callback',
+                                                'intent_subscribe', 'intent_unsubscribe'], 'keyboard': True,
+                             'inline_keyboard': True,
+                             'carousel': True, 'lang_id': 0}},
         'group_id': 205318557, 'event_id': '1d104ee315dc2c2d2ca1322b8aec6b3daf8e1f47'}
 
     def test_run(self):
+        # код ниже нужен для того, чтобы переопределить метод listen (bot61). Нужно заменить объект long_poller (bot52)
+        # определить у него метод listen и сделать так чтобы он возвращал какой-нибудь список , т.к. listen является
+        # генератором и вовзращает список event
+
+        # сколько раз мы вернем наш event, примерное значение
         count = 5
+        # создадим любой словарь, оторый будет служить заменой реальным событиям (event, сообщениям от пользователей)
         obj = {'a': 1}
-        events = [obj] * count  # [obj, obj, ...]
+        # создадим список словарей, типа проверим несколько событий в данном случае, как будто нам написали 5 пользоват.
+        events = [obj] * count  # [obj, obj, obj, obj, obj]
+        # это замена long_poller (bot52) на мок, который будет возвращать список ивентов
+        # и работать в bot.on_event = Mock()
         long_poller_mock = Mock(return_value=events)
+        # поскольку только метод .listen возвращает нам список, его тоже нужно заменить на мок
         long_poller_listen_mock = Mock()
+        # вызываем метод .listen у мока и присваеваем ему другой объект мок, который уже возвращает нам event
         long_poller_listen_mock.listen = long_poller_mock
 
+        # функция патч принимает объект, который ей нужно пропатчить, т.е. заменить на мок объект, чтобы они
+        # не вызывались в реальности. Патчи работают с помощью фунции with. Объект api (bot56) тоже стал моком,
+        # т.к. вызывался от vk_api.VkApi его не надо отдельно патчить.
         with patch('bot.vk_api.VkApi'):
+            # return_value забирает у класса VkBotLongPoll значение и меняет его на новое, т.е. говорит ему
+            # что VkBotLongPoll теперь будет long_poller_listen_mock
             with patch('bot.VkBotLongPoll', return_value=long_poller_listen_mock):
+                # создали объект от класса бот с пустыми параметрами (без group id и token)
                 bot = Bot('', '')
+                # по скольку в методе run вызывается метод on_event мы его заменим на мок объект,
+                # т.к. в нем может быть куча зависемостей, вызовов и т.д.
                 bot.on_event = Mock()
+                # запускаем метод run у класса bot чтобы проверит, что он работает
                 bot.run()
 
+                # проверяет был ли вообще вызван метод on_event
                 bot.on_event.assert_called()
+                # убедиться что вызов который был сделан, он сделан с помощью нашего объекта, события, event
                 bot.on_event.assert_any_call(obj)
+                # проверка что вызов on_event происходил именно с нашими событиями (event) и столько раз сколько надо,
+                # в данном случае 5 раз
                 assert bot.on_event.call_count == count
 
     def test_on_event(self):
+        # только в случае если класс не делает никаких вызовов во вне (только метод __init__)
         event = VkBotMessageEvent(raw=self.RAW_EVENT)
 
+        # создадим мок который нам заменит все входящие event
         send_mock = Mock()
 
+        # снова патчим объекты bot.vk_api.VkApi и bot.VkBotLongPoll
         with patch('bot.vk_api.VkApi'):
             with patch('bot.VkBotLongPoll'):
+                # создали объект от класса бот с пустыми параметрами (без group id и token)
                 bot = Bot('', '')
+                # bot.api тоже будет моком
                 bot.api = Mock()
+                # если кто-то захочет у bot.api (который уже мок) вызвать методы .messages.send то он
+                # наткнется на другой мок
                 bot.api.messages.send = send_mock
 
+                # вызовем метод on_event который принимает вполне реальный event из примера
                 bot.on_event(event)
 
+        # проверим что событие было вызвано лишь один раз и с теми параметрами которые мы ожидаем (bot84)
         send_mock.assert_called_once_with(
             message=self.RAW_EVENT['object']['message']['text'],
             random_id=ANY,
             peer_id=self.RAW_EVENT['object']['message']['peer_id'])
+
+# для того чтобы посчитать покрытие кода тестами (т.е. сколько строк из кода были реально исполенны)
+# используется утилита coverage (pip install coverage). В консоли запускаем тесты с утилитой coverage:
+# coverage run --source=bot -m unittest. Смотреть покрытие тестами только файл bot. Этот вызов создаст файл coverage
+# Далее смотрим отчет: coverage report -m.
